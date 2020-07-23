277b3c5d31ee2fddee7c33b8ed0a07ccf4b5acd8
Tony Atkins <duhrer@gmail.com>
2020-07-23 13:45:00 +0200

    Added initial version of react sliding puzzle demo.

---
+++ demos/react-launchpad-slide-puzzle/precache-manifest.d11d6a0936e70ee0e6c618fe31a46749.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fccded71e094cb7342e0a0014360892f",
+    "url": "/demos/react-launchpad-slide-puzzle/index.html"
+  },
+  {
+    "revision": "ce8551fec6c4d252c958",
+    "url": "/demos/react-launchpad-slide-puzzle/static/css/main.c1ce9c80.chunk.css"
+  },
+  {
+    "revision": "0a6cd4b3b266db5818d1",
+    "url": "/demos/react-launchpad-slide-puzzle/static/js/2.2eb91dc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demos/react-launchpad-slide-puzzle/static/js/2.2eb91dc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce8551fec6c4d252c958",
+    "url": "/demos/react-launchpad-slide-puzzle/static/js/main.c73ddc36.chunk.js"
+  },
+  {
+    "revision": "7dbf8d78e7aba58dd574",
+    "url": "/demos/react-launchpad-slide-puzzle/static/js/runtime-main.27936046.js"
+  }
+]);
